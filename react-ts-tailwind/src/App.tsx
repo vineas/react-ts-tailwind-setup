d3960fe7dd757b731582f832b450292f6fa3ec0c
vineas <alvienasyandika@gmail.com>
2023-12-13 12:47:45 +0700

    first commit

---
+++ react-ts-tailwind/src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+    {/* Test aja */}
+      <div className="flex ...">
+        <div className="border grow h-14 text-center">
+          01
+        </div>
+        <div className="border grow-0 h-14 text-center">
+          02
+        </div>
+        <div className="border grow h-14 text-center">
+          03
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
